3a9c4b68c3fdf8c20f4c7ff5c5e973f454b7771b
Tobias Järvelöv <tobias@jarvelov.se>
2015-08-05 16:15:31 +0200

    Initial commit

---
+++ controller/controller.php
@@ -0,0 +1,56 @@
+<?php
+
+Class RestController {
+
+    public function addUser($name, $username, $password, $email) {
+        echo $name;
+    }
+
+    public function getAllUsers() {
+
+    }
+
+    public function getUser($username) {
+
+    }
+
+    function __construct() {
+        if( class_exists('Flight') ) {
+            //Hook up all REST requests to functions
+            Flight::route('/', function() {
+                $this->init();
+            });
+
+            //Add a new user
+            Flight::route('/users/add_user/@name/@username/@password/@email', function( $name, $username, $password, $email ) {
+                $this->addUser( $name, $username, $password, $email );
+            } );
+
+            //Get user by username
+            Flight::route('/users/get_user/@username', function( $username ) {
+                $this->getUser( $username );
+            });
+
+            //Get all users
+            Flight::route('/users/get_user/@username', function() {
+                $this->getAllUsers();
+            });
+
+            //Ready for take off!
+            $this->start();
+        } else {
+            echo "Error: Flight framework is not initalized!";
+        }
+    }
+
+    public function init() {
+
+    }
+
+    public function start() {
+        Flight::start();
+    }
+
+}
+
+ ?>
