8df1886e18f0c1f4a51e7e61d6b6c94b296349e8
Tobias Järvelöv <tobias@jarvelov.se>
2015-08-05 21:55:36 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ REST.controller(
         }
 
         $scope.reset = function() {
+            $scope.action = false;
             $scope.hideAddUserForm = true;
             $scope.hideGetUserForm = true;
             $scope.progressHide = true;
