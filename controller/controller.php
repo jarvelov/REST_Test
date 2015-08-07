cd731f5fd23d07195cc397e87c235cf7b57fd01f
Tobias Järvelöv <tobias@jarvelov.se>
2015-08-07 20:09:18 +0200

    Update

--- controller/controller.php
+++ controller/controller.php
@@ -20,7 +20,7 @@ Class RestController {
             });
 
             //Get all users
-            Flight::route('/users/get_all_users/@username', function() {
+            Flight::route('/users/get_all_users', function() {
                 $this->getAllUsers();
             });
         } else {
