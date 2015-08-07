46b3495fc4c044bf844dd0653520df19ca460144
Tobias Järvelöv <tobias@jarvelov.se>
2015-08-07 20:49:06 +0200

    Add debug

--- controller/dbconnection.php
+++ controller/dbconnection.php
@@ -18,6 +18,7 @@ Class DatabaseConnection {
         try {
             $this->db->openDatabase();
         } catch(Exception $e) { //Database does not seem to exist
+            var_dump($e);
             throw new Exception ('General error. Please check permissions', 1); //make this prettier
         }
     }
@@ -35,6 +36,7 @@ Class DatabaseConnection {
 
             $result = $stmt->execute();
         } catch(Exception $e) {
+            var_dump($e);
             throw new Exception("Error saving user to database!", 1);
         }
 
