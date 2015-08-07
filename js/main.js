8aa0c6e30bfe84ae78cbe6f8fc6576383a97b9d5
Tobias Järvelöv <tobias@jarvelov.se>
2015-08-07 21:13:01 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -97,7 +97,7 @@ REST.controller(
 
         $scope.sendPost = function() {
             $scope.data = 'test';
-            $scope.url = '/get_user/';
+            $scope.url = '/get_user/JohnS';
             var data = JSON.stringify( {data: $scope.data} );
             $http.post($scope.url, data).success(function(data, status) {
                 console.log(data, status);
