b788b828740c2b5c97f0e38f9e7827eb87e8e823
Tobias Järvelöv <tobias@jarvelov.se>
2015-08-07 20:27:14 +0200

    Update error text

--- controller/controller.php
+++ controller/controller.php
@@ -42,7 +42,7 @@ Class RestController {
         if( $result['success'] === true ) {
             $message = $result['message'];
         } else {
-            $message = 'Error: Unable to retrieve a list of all users!';
+            $message = 'Error: Unable to retrieve specified user!';
         }
 
         $this->output($message);
