01963e7f8cf681fc25cb1b1cbacc0b98056596c3
Tobias Järvelöv <tobias@jarvelov.se>
2015-08-07 18:59:01 +0200

    Update

--- controller/database.php
+++ controller/database.php
@@ -1,45 +1,47 @@
 <?php
 
-Class Database extends SQLite3 {
-    protected $db, $dbpath;
-    function __construct($database) {
-        $this->dbpath = $database;
-    }
+if(class_exists('SQLite3')) {
+    Class Database extends SQLite3 {
+        protected $db, $dbpath;
+        function __construct($database) {
+            $this->dbpath = $database;
+        }
 
-    public function open() {
-        try {
-            $this->db = $this->open( $this->database );
-        } catch(Exception $e) {
-            throw new Exception("Error Opening Database. Does it exist?", 1);
+        public function open() {
+            try {
+                $this->db = $this->open( $this->database );
+            } catch(Exception $e) {
+                throw new Exception("Error Opening Database. Does it exist?", 1);
+            }
+        }
+
+        public function create() {
+            $this->db = new SQLite3( $this->dbpath );
+            $this->fill();
+        }
+
+        private function fill() {
+            $table_query = 'CREATE TABLE users IF NOT EXIST (
+                id mediumint(9) AUTO_INCREMENT,
+                username VARCHAR(255),
+                name VARCHAR(255),
+                password VARCHAR(255),
+                email VARCHAR(255),
+                UNIQUE KEY id (id)
+            );';
+
+            $table_stmt = $this->db->prepare($table_query);
+            $table_stmt->execute();
+
+            $data_query = 'INSERT INTO users (id, username, name, password, email) VALUES (
+                    1,"JohnS","John Simonson","SuperS3cure","john.simonson@example.com",
+                    2,"AliceC","Alice Cooper","SpookyFella97","alice.cooper@example.com",
+                    1,"StevenT","Steven Tyler","RockOn4Lyfe","steven.tyler@example.com",
+                )';
+
+            $data_stmt = $this->db->prepare($data_query);
+            $data_stmt->execute();
         }
     }
-
-    public function create() {
-        $this->db = new SQLite3( $this->dbpath );
-        $this->fill();
-    }
-
-    private function fill() {
-        $table_query = 'CREATE TABLE users IF NOT EXIST (
-            id mediumint(9) AUTO_INCREMENT,
-            username VARCHAR(255),
-            name VARCHAR(255),
-            password VARCHAR(255),
-            email VARCHAR(255),
-            UNIQUE KEY id (id)
-        );';
-
-        $table_stmt = $this->db->prepare($table_query);
-        $table_stmt->execute();
-
-        $data_query = 'INSERT INTO users (id, username, name, password, email) VALUES (
-                1,"JohnS","John Simonson","SuperS3cure","john.simonson@example.com",
-                2,"AliceC","Alice Cooper","SpookyFella97","alice.cooper@example.com",
-                1,"StevenT","Steven Tyler","RockOn4Lyfe","steven.tyler@example.com",
-            )';
-
-        $data_stmt = $this->db->prepare($data_query);
-        $data_stmt->execute();
-    }
 }
- ?>
+?>
--- model/model.php
+++ model/model.php
@@ -2,80 +2,85 @@
 
 Class Model {
     function __construct($errors) {
+        var_dump($errors);
         $this->errors = $errors;
+        $this->init();
+    }
+
+    function init() {
+        ?>
+        <!DOCTYPE html>
+        <html ng-app="REST">
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <title>REST test</title>
+                <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+                <link rel="stylesheet" href="css/main.css">
+                <script src="lib/jquery/js/jquery.js"></script>
+                <script src="lib/bootstrap/js/bootstrap.min.js"></script>
+                <script src="lib/angularjs/js/angular.js"></script>
+                <script src="js/main.js"></script>
+            </head>
+            <body>
+                <div class="container" ng-controller="mainController">
+                    <div id="error-container" class="row">
+                        <div id="errors" class="col-xs-12">
+                            <?php
+                                foreach($this->errors as $error) {
+                                    echo '<div id="error" class="alert alert-danger">' . $error . '</div>';
+                                }
+                            ?>
+                        </div>
+                    </div><!-- /#error-container -->
+                    <div id="option-container" class="row topUp" >
+                        <div class="col-xs-12">
+                            <h3>Options</h3>
+                        </div>
+                        <div class="col-xs-4">
+                            <button id="addUser" class="btn btn-block btn-default" ng-click="formAddUser()">Add User</button>
+                        </div>
+                        <div class="col-xs-4">
+                            <button id="getUser" class="btn btn-block btn-default" ng-click="formGetUser()">Get A User</button>
+                        </div>
+                        <div class="col-xs-4">
+                            <button id="getAllUsers" class="btn btn-block btn-default" ng-click="getAllUsers()">Get All Users</button>
+                        </div>
+                    </div><!-- /#option-container -->
+
+                    <div id="parameter-container" class="row topUp">
+                        <div class="col-xs-12">
+                            <h3>Required parameters</h3>
+                            <div class="alert alert-info" ng-hide="hideInfo"><span class="glyphicon glyphicon-exclamation-sign"></span> Click any of the buttons up top to continue.</div>
+                        </div>
+                        <div class="form-container" ng-repeat="form in Forms">
+                            <form id="{{form.name}}" ng-hide="form.functions.isHidden()">
+                                <div class="form-group col-xs-12" ng-repeat="input in form.inputs">
+                                    <label for="{{input.label}}">{{input.label}}</label>
+                                    <input id="{{input.label}}" type="{{input.type}}" class="form-control" ng-pattern="input.pattern" name="{{input.label}}" />
+                                </div><!-- /.form-group -->
+                            </form><!-- /form -->
+                            <div id="action-button" class="col-xs-12" ng-hide="form.functions.isHidden()">
+                                <button class="btn btn-block btn-primary">{{form.action}}</button>
+                            </div><!-- /#action-button -->
+                        </div>
+                    </div><!-- /#parameter-container -->
+
+                    <div id="result-container" class="row topUp">
+                        <div id="result-title" class="col-xs-12">
+                            <h3>Results</h3>
+                            <div class="alert alert-info" ng-hide="hideInfo"><span class="glyphicon glyphicon-exclamation-sign"></span> Results will show here.</div>
+                        </div><!-- /#result-title -->
+                        <div id="progress-container" class="col-xs-12" ng-hide="hideProgress">
+                            <div class="progress">
+                                <div id="progressbar" class="progress-bar" role="progressbar">
+                                </div><!-- /#progressbar -->
+                            </div><!-- /.progress -->
+                        </div><!-- /#progress-container -->
+                    </div><!-- /.row -->
+                </div><!-- /.container -->
+            </body>
+        </html>
+        <?php
     }
 }
-
- ?>
-
-<!DOCTYPE html>
-<html ng-app="REST">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>REST test</title>
-        <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="lib/jquery/js/jquery.js"></script>
-        <script src="lib/bootstrap/js/bootstrap.min.js"></script>
-        <script src="lib/angularjs/js/angular.js"></script>
-        <script src="js/main.js"></script>
-    </head>
-    <body>
-        <div class="container" ng-controller="mainController">
-            <div id="error-container" class="row">
-                <div class="col-xs-12">
-                    <?php
-                        foreach($this->errors as $error) {
-                            echo '<div class="alert alert-danger">' . $error . '</div>';
-                        }
-                    ?>
-                </div>
-            </div><!-- /#error-container -->
-            <div id="option-container" class="row topUp" >
-                <div class="col-xs-12">
-                    <h3>Options</h3>
-                </div>
-                <div class="col-xs-4">
-                    <button id="addUser" class="btn btn-block btn-default" ng-click="formAddUser()">Add User</button>
-                </div>
-                <div class="col-xs-4">
-                    <button id="getUser" class="btn btn-block btn-default" ng-click="formGetUser()">Get A User</button>
-                </div>
-                <div class="col-xs-4">
-                    <button id="getAllUsers" class="btn btn-block btn-default" ng-click="getAllUsers()">Get All Users</button>
-                </div>
-            </div><!-- /#option-container -->
-
-            <div id="parameter-container" class="row topUp">
-                <div class="col-xs-12">
-                    <h3>Required parameters</h3>
-                    <div class="alert alert-info" ng-hide="hideInfo"><span class="glyphicon glyphicon-exclamation-sign"></span> Click any of the buttons up top to continue.</div>
-                </div>
-                <div class="form-container" ng-repeat="form in Forms">
-                    <form id="{{form.name}}" ng-hide="form.functions.isHidden()">
-                        <div class="form-group col-xs-12" ng-repeat="input in form.inputs">
-                            <label for="{{input.label}}">{{input.label}}</label>
-                            <input id="{{input.label}}" type="{{input.type}}" class="form-control" ng-pattern="input.pattern" name="{{input.label}}" />
-                        </div><!-- /.form-group -->
-                    </form><!-- /form -->
-                    <div id="action-button" class="col-xs-12" ng-hide="form.functions.isHidden()">
-                        <button class="btn btn-block btn-primary">{{form.action}}</button>
-                    </div><!-- /#action-button -->
-                </div>
-            </div><!-- /#parameter-container -->
-
-            <div id="result-container" class="row topUp">
-                <div id="result-title" class="col-xs-12">
-                    <h3>Results</h3>
-                    <div class="alert alert-info" ng-hide="hideInfo"><span class="glyphicon glyphicon-exclamation-sign"></span> Results will show here.</div>
-                </div><!-- /#result-title -->
-                <div id="progress-container" class="col-xs-12" ng-hide="hideProgress">
-                    <div class="progress">
-                        <div id="progressbar" class="progress-bar" role="progressbar">
-                        </div><!-- /#progressbar -->
-                    </div><!-- /.progress -->
-                </div><!-- /#progress-container -->
-            </div><!-- /.row -->
-        </div><!-- /.container -->
-    </body>
-</html>
+?>
