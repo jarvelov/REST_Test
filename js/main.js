1ae4dc56e23ce65a5b47bdb6d83991afb4437bef
Tobias Järvelöv <tobias@jarvelov.se>
2015-08-07 23:11:29 +0200

    Update

--- js/main.js
+++ js/main.js
@@ -37,8 +37,8 @@ REST.controller(
                     },
                     email:{
                         pattern:"",//"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}",
-                        slug:"em",
-                        type:"text",
+                        slug:"email",
+                        type:"email",
                         label:"E-mail"
                     }
                 },
